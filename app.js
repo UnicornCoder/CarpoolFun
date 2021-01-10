4e29f4061dba1c498ed7c38e6aa89e3b566ca7db
UnicornCoder <76819758+UnicornCoder@users.noreply.github.com>
2021-01-10 12:04:36 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,45 @@
+
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './pages/login';
+import Login1 from './pages/login1';
+import SignUp from './pages/Signup';
+import SignUp1 from './pages/Signup1';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Login" component={Login1} />
+        <Stack.Screen name="SignUp" component={SignUp1} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
